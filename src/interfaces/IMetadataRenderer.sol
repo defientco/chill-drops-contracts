045c1eaac6f91a290584f669394e594658f05679
sameer rasa <crazzywizard@gmail.com>
2022-12-05 18:14:26 -0500

    prettier fix

--- src/interfaces/IMetadataRenderer.sol
+++ src/interfaces/IMetadataRenderer.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.10;
 
 interface IMetadataRenderer {
     function tokenURI(uint256) external view returns (string memory);
+
     function contractURI() external view returns (string memory);
+
     function initializeWithData(bytes memory initData) external;
 }
