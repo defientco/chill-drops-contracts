2a59e2a0caaadd1076ec0fdfb2d0c4a26afef56b
sameer rasa <crazzywizard@gmail.com>
2022-12-05 22:32:09 -0500

    updated address and deploy script

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -2,11 +2,49 @@ import { deployAndVerify } from './contract.mjs';
 import { writeFile } from 'fs/promises';
 import dotenv from 'dotenv';
 import esMain from 'es-main';
-
+import yargs from 'yargs';
+const argv = yargs(process.argv.slice(2)).option('allowList', {
+  alias: 'a',
+  type: 'boolean',
+  description: 'Deploy Allow List Contracts'
+}).argv;
 dotenv.config({
   path: `.env.${process.env.CHAIN}`
 });
+export async function setupAllowListContracts() {
+  const zoraERC721TransferHelperAddress = process.env.ZORA_ERC_721_TRANSFER_HELPER_ADDRESS;
 
+  if (!zoraERC721TransferHelperAddress) {
+    throw new Error('erc721 transfer helper address is required');
+  }
+
+  console.log('deploying Erc721Drop');
+  const allowListDropContract = await deployAndVerify('src/AllowListDrop.sol:AllowListDrop', [
+    zoraERC721TransferHelperAddress
+  ]);
+  const allowListDropContractAddress = allowListDropContract.deployed.deploy.deployedTo;
+  console.log('deployed drop contract to ', allowListDropContractAddress);
+  console.log('deploying drops metadata');
+  const allowListMetadataContract = await deployAndVerify(
+    'src/metadata/AllowListMetadataRenderer.sol:AllowListMetadataRenderer',
+    []
+  );
+  const allowListMetadataAddress = allowListMetadataContract.deployed.deploy.deployedTo;
+  console.log('deployed drops metadata to', allowListMetadataAddress);
+
+  console.log('deploying creator implementation');
+  const creatorImpl = await deployAndVerify('src/AllowListNFTCreatorV1.sol:AllowListNFTCreatorV1', [
+    allowListDropContractAddress,
+    allowListMetadataAddress
+  ]);
+  console.log('deployed creator implementation to', creatorImpl.deployed.deploy.deployedTo);
+
+  return {
+    allowListDropContract,
+    allowListMetadataContract,
+    creatorImpl
+  };
+}
 export async function setupContracts() {
   const zoraERC721TransferHelperAddress = process.env.ZORA_ERC_721_TRANSFER_HELPER_ADDRESS;
 
@@ -28,32 +66,23 @@ export async function setupContracts() {
   const dropMetadataAddress = dropMetadataContract.deployed.deploy.deployedTo;
   console.log('deployed drops metadata to', dropMetadataAddress);
 
-  console.log('deploying allowlist metadata renderer');
-  const allowlistMetadataContract = await deployAndVerify(
-    'src/metadata/AllowListMetadataRenderer.sol:AllowListMetadataRenderer',
-    []
-  );
-  const allowlistMetadataAddress = allowlistMetadataContract.deployed.deploy.deployedTo;
-  console.log('deployed allowlist metadata to', allowlistMetadataAddress);
-
   console.log('deploying creator implementation');
   const creatorImpl = await deployAndVerify('src/ZoraNFTCreatorV1.sol:ZoraNFTCreatorV1', [
     dropContractAddress,
-    dropMetadataAddress,
-    allowlistMetadataAddress
+    dropMetadataAddress
   ]);
   console.log('deployed creator implementation to', creatorImpl.deployed.deploy.deployedTo);
 
   return {
     dropContract,
     dropMetadataContract,
-    allowlistMetadataContract,
     creatorImpl
   };
 }
 
 async function main() {
-  const output = await setupContracts();
+  console.log(argv);
+  const output = argv.a ? await setupAllowListContracts() : await setupContracts();
   const date = new Date().toISOString().slice(0, 10);
   writeFile(`./deployments/${date}.${process.env.CHAIN}.json`, JSON.stringify(output, null, 2));
 }
